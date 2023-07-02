07857aa7969702ee0cd476dcc236374e1039809e
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-02 22:59:54 +0530

    Update PageHeader.js

--- src/PageHeader.js
+++ src/PageHeader.js
@@ -3,20 +3,11 @@ import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Home from "./Home";
-import AboutUs from "./AboutUs";
-import Facilities from "./Facilities";
-
 import './App.css';
 import logo from './logo.svg';
 
 function PageHeader() {
   return (
-    <HashRouter>
-    <Route exact path="/" element={<Home />}/>
-          <Route path="/AboutUs" element={<AboutUs />} />
-          <Route path="/Facilities" element={<Facilities />} />
         
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid style={{
@@ -24,13 +15,13 @@ function PageHeader() {
         }}>
         <Navbar.Brand href="https://saad-rahman-warsi.github.io/sciplanet/" style={{color: '#33CEFF'}}>
             <img alt="" src={logo} width="60"  className="App-logo"  />{' '}
-            <b>Sci Planet</b>
+            <b>SciPlanet</b>
         </Navbar.Brand>
 
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav" style={{color: 'White' }}>
           <Nav className="me-auto">
-            <Nav.Link to="/AboutUs" style={{color: 'White'}}>About Us</Nav.Link>
+            <Nav.Link href="https://saad-rahman-warsi.github.io/sciplanet/#/AboutUs" style={{color: 'White'}}>About Us</Nav.Link>
 
             <NavDropdown title={ <span className="text-white my-auto">Explore</span> } id="basic-nav-dropdown">
               <NavDropdown.Item to="/Facilities">Learn Experiments</NavDropdown.Item>
@@ -51,7 +42,6 @@ function PageHeader() {
       </Container>
     </Navbar>
     
-    </HashRouter>
   );
 }
 
