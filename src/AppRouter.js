ae556e5aedf6887a44bb5546040df1fe87d870cc
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-02 18:13:12 +0530

    Update AppRouter.js

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -2,7 +2,7 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
 import AboutUs from "./AboutUs";
 import Facilities from "./Facilities";
-import {Outlet} from "react-router-dom";
+
 
 function AppRouter() {
   return (
@@ -10,13 +10,12 @@ function AppRouter() {
     <HashRouter>
       <Routes>
           <Route path="/" element={<Home />}>
-          <Route index element={<Home />} />
+          
           <Route path="/AboutUs" element={<AboutUs />} />
           <Route path="/Facilities" element={<Facilities />} />
           </Route>
       </Routes>
     </HashRouter>
-    <Outlet />
     </div>
   );
 }
