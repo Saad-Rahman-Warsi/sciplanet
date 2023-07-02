cec865c776b92a326a7004ddadc373e6e04958d7
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-02 16:28:08 +0530

    Update Layout.js

--- src/Layout.js
+++ src/Layout.js
@@ -1,9 +1,25 @@
-import Container from 'react-bootstrap/Container';
-
-function Layout() {
+import { Outlet, Link } from "react-router-dom";
+ 
+const Layout = () => {
   return (
-    <div> <h1> Layout </h1> </div>
-  );
-}
+    <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/blogs">Blogs</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+    </>
+  )
+};
 
 export default Layout;
