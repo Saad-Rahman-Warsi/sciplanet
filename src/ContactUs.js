1a81ceb65173cfd143fc7266b639f3bbd9aab514
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-03 07:47:31 +0530

    Update ContactUs.js

--- src/ContactUs.js
+++ src/ContactUs.js
@@ -11,23 +11,25 @@ function ContactUs() {
   return (
     <div>
       <br /><h3> Contact Us </h3><br /><br />
+      <Col xs={3}></Col>
       <Col xs={6}>
-      <Form>
-      <Form.Group className="mb-3" controlId="contactUsForm.ControlInput1">
-        <Form.Label>Name</Form.Label>
-        <Form.Control type="email" placeholder="First Name Last Name" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="contactUsForm.ControlInput1">
-        <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="name@example.com" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="contactUsForm.ControlTextarea1">
-        <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows={5} />
-      </Form.Group>
-      <Button as="input" type="submit" value="Submit" />{' '}
-    </Form>
+        <Form>
+        <Form.Group className="mb-3" controlId="contactUsForm.ControlInput1">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="email" placeholder="First Name Last Name" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="contactUsForm.ControlInput1">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" placeholder="name@example.com" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="contactUsForm.ControlTextarea1">
+          <Form.Label>Message</Form.Label>
+          <Form.Control as="textarea" rows={5} />
+        </Form.Group>
+          <Button as="input" type="submit" value="Submit" />{' '}
+        </Form>
     </Col>
+    <Col xs={3}></Col>
      
     <PageFooter />
       </div>
