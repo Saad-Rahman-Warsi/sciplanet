3cb83d176f4dc91da004cc1a4d1e3d64c42d74fc
Saad-Rahman-Warsi <saadrahmanwarsi@gmail.com>
2023-07-03 06:46:29 +0530

    u

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,11 +1,49 @@
 import Container from 'react-bootstrap/Container';
 import PageHeader from './PageHeader.js';
 
+import abt1 from './aboutus1.jpg';
+import abt2 from './aboutus2.jpg';
+
 function AboutUs() {
   return (
     <div>
   
      <h1> AboutUs </h1>
+     <Container fluid style={{
+          backgroundColor: '#F2F4F4', textAlign: 'left'
+        }}>
+
+      <Row>
+        <Col xs={8}>
+          <h3>Our Story</h3>
+          <p>Our company was born out of a profound belief in the power of science to ignite curiosity, 
+          transform lives, and shape a better future for all. We have embarked on a remarkable journey to 
+          make the wonders of scientific laboratories 
+          accessible to the public. Our story begins with a vision to bridge the gap between the
+           scientific community and the wider society. We initially started with helping the student
+           in understanding the experiments better. Today we have expanded this to sample analysis 
+           and advanced equipment.</p>
+        </Col>
+        <Col xs={4}>
+          <img className="d-block w-100" src={abt1} alt="Our Story" />
+        </Col>
+      </Row>
+
+      <Row>
+        <Col xs={4}>
+          <img className="d-block w-100" src={abt2} alt="Our Story" />
+        </Col>
+        <Col xs={8}>
+          <h3>Our Mission</h3>
+          <p>Our Mission to democratize access to scientific laboratories and inspire a passion for 
+          discovery in the public. Our company is driven by a deep-rooted commitment to make scientific 
+          exploration inclusive and accessible to all. We strive to provide a welcoming space where 
+          individuals from diverse backgrounds can engage in hands-on experiments, gain practical scientific 
+          knowledge, and foster a love for learning.</p>
+        </Col>
+      </Row>
+
+    </Container>
       </div>
   );
 }
